a8ad038a92ae5625d0107ccd6ddb59131bd397b7
Nouhailaanissi <nouhailaanissi06@gmail.com>
2025-08-01 13:13:08 +0200

    premier commit

---
+++ vite/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ComponentA from "./Compenents/ComponentA";
+
+const App = () => {
+  const name = "Nouhaila";
+  return <ComponentA name={name} />;
+};
+
+export default App;
